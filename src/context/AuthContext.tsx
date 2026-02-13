e31679785a8aef52edb8549061efcc064c8ddc49
jotavitorz <joaovitorxz812@gmail.com>
2026-02-12 22:48:49 -0300

    Primeiro commit - projeto WebCars

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,68 @@
+import { type ReactNode, createContext, useState, useEffect} from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../services/firebaseConnection";
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+type AuthContextData = {
+    signed: boolean;
+    loadingAuth: boolean;
+    handleInfoUser: ({email, name, uid}: UserProps) => void;
+    user: UserProps | null;
+}
+
+interface UserProps {
+    uid: string;
+    name: string | null;
+    email: string | null;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({children}: AuthProviderProps){
+    const [user, setUser] = useState<UserProps | null>(null);
+    const [loadingAuth, setLoadingAuth] = useState(true);
+
+    useEffect(() => {
+
+        const unsub = onAuthStateChanged(auth, (user) => {
+            if(user){
+                // Tem user logado
+                setUser({
+                    uid: user.uid,
+                    name: user?.displayName,
+                    email: user?.email,
+                })
+                setLoadingAuth(false);
+            }else {
+                // nao tem user logado
+                setUser(null);
+                setLoadingAuth(false)
+            }
+        })
+
+        return () => {
+            unsub();
+        }
+
+    }, [])
+
+    function handleInfoUser({email, name, uid}: UserProps){
+        setUser({
+            name,
+            email,
+            uid
+        })
+    }
+
+    return (
+        <AuthContext.Provider value={{ signed: !!user, loadingAuth, handleInfoUser, user }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
+
